d1a324f8a85276ea0448bde29480564a57d1403a
Bryant6 <1963899588@qq.com>
2020-02-22 21:06:48 +0800

    springboot框架基本搭建及JSP的支持

---
+++ src/main/java/com/wang/springboot/web/HelloController.java
@@ -0,0 +1,17 @@
+package com.wang.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(Model m) {
+        m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
+        return "hello";
+    }
+}
